a704d28c70dd3dac91dbd53a97b7cca722675ceb
namyoonjeong <jwudice7@naver.com>
2017-10-18 23:00:04 +0900

    fr

---
+++ 6Week/21561043_남윤정/js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $('ul#menu_page').hide();
+    
+    $('a#menu_bt').click(function(){
+        $('ul#menu_page').toggle();
+    });
+
+    $('li#poster1').click(function(){
+       $('section.poster1').show();
+       $('section.poster2').hide();
+       $('section.poster3').hide();
+       $('ul#menu_page').hide();
+    });
+
+    $('li#poster2').click(function(){
+        $('section.poster2').show();
+        $('section.poster1').hide();
+        $('section.poster3').hide();
+        $('ul#menu_page').hide();
+     });
+
+     $('li#poster3').click(function(){
+        $('section.poster3').show();
+        $('section.poster1').hide();
+        $('section.poster2').hide();
+        $('ul#menu_page').hide();
+     });
+});
