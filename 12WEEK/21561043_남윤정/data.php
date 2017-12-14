2e6ffa9718758bd7fd3714b08b59b12c0efbb26b
namyoonjeong <jwudice7@naver.com>
2017-12-14 20:22:43 +0900

    추가제출합니다

---
+++ 12WEEK/21561043_남윤정/data.php
@@ -0,0 +1,146 @@
+<?php
+//mysql
+//query = 'select'
+
+$data = array();
+array_push($data, array(
+    'id'     => 'ryonghwon',
+    'number' => '20170004',
+    'name'   => '김용원',
+    'role'   => '교수'
+));
+array_push($data, array(
+    'id'     => 'Limjiwoong',
+    'number' => '21247167',
+    'name'   => '임지웅',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'ma-seung-hyun',
+    'number' => '21347072',
+    'name'   => '마승현',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'MoonWooJoo',
+    'number' => '21461049',
+    'name'   => '문우주',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'minji3',
+    'number' => '21561006',
+    'name'   => '권민지',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'namyoonjeong',
+    'number' => '21561043',
+    'name'   => '남윤정',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'parksunjoo',
+    'number' => '21561050',
+    'name'   => '박선주',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'tongtong9513',
+    'number' => '21561058',
+    'name'   => '박희영',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'bomyks',
+    'number' => '21561074',
+    'name'   => '오이슬',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'Eunjilee96',
+    'number' => '21561096',
+    'name'   => '이은지',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'ab12b1125',
+    'number' => '21561098',
+    'name'   => '이준영',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'jeonhanna',
+    'number' => '21561102',
+    'name'   => '전한나',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'mmyuuu',
+    'number' => '21661013',
+    'name'   => '김명유',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'kimmisong',
+    'number' => '21661014',
+    'name'   => '김미송',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'jadejun',
+    'number' => '21661021',
+    'name'   => '김소연',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'legavin1023',
+    'number' => '21661035',
+    'name'   => '김희선',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'Yoon096',
+    'number' => '21661084',
+    'name'   => '윤수영',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'seojj',
+    'number' => '21661086',
+    'name'   => '이서진',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'adorablej',
+    'number' => '21661093',
+    'name'   => '이승주',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'itsmedain',
+    'number' => '21661111',
+    'name'   => '전다인',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'malang97',
+    'number' => '21661114',
+    'name'   => '정세림',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'whamoop',
+    'number' => '21661115',
+    'name'   => '정우민',
+    'role'   => '학생'
+));
+array_push($data, array(
+    'id'     => 'ChoeYunJeong',
+    'number' => '21661131',
+    'name'   => '최윤정',
+    'role'   => '학생'
+));
+header("Content-type: application/json");
+echo json_encode($data);
+?>
